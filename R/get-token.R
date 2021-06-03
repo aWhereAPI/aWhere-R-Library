97afcc321251fcbf6fc378cba89de7baf0f9f5d4
colinloftin-awhere <colinloftin-awhere@users.noreply.github.com>
2021-06-03 16:40:50 -0400

    Updated environment assignment

--- R/get-token.R
+++ R/get-token.R
@@ -56,7 +56,7 @@ get_token <- function(uid, secret, use_environment = TRUE, apiAddress = "api.awh
     if (use_environment) {
       if (exists('awhereEnv75247') == FALSE) {
         awhereEnv75247 <- new.env()
-        assign('awhereEnv75247',awhereEnv75247,envir = baseenv())
+        assign('awhereEnv75247',awhereEnv75247,envir = .GlobalEnv)
         rm(awhereEnv75247)
       }
       
